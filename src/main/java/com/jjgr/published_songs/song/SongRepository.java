c2707863973ab8c04eb1b5ad2374b9eb449c8c1c
Jesús Gaytán Ramírez <jesusgaytanramirez144@gmail.com>
2024-06-12 14:54:29 -0600

    Implementación de SongRepository

---
+++ src/main/java/com/jjgr/published_songs/song/SongRepository.java
@@ -0,0 +1,13 @@
+package com.jjgr.published_songs.song;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SongRepository extends JpaRepository<Song, Long>{
+    @Query("SELECT s FROM Song s WHERE s.name = ?1")
+    Optional<Song> findByName(String name);
+}

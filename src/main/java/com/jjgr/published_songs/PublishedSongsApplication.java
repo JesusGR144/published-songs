398e9a71d2f6bd2354a75bac590ca8fb7b26d994
Jesús Gaytán Ramírez <jesusgaytanramirez144@gmail.com>
2024-06-11 23:18:42 -0600

    Creando el proyecto pusblished songs

---
+++ src/main/java/com/jjgr/published_songs/PublishedSongsApplication.java
@@ -0,0 +1,13 @@
+package com.jjgr.published_songs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublishedSongsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublishedSongsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jjgr/published_songs/PublishedSongsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjgr.published_songs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublishedSongsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
